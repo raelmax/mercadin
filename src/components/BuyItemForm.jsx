7b7ce4e8d2517d6ebe861bab38068930670ce0de
Rael Max <rael.nascimento@corp.globo.com>
2016-11-15 23:28:58 -0200

    Add styles to dialog

--- src/components/BuyItemForm.jsx
+++ src/components/BuyItemForm.jsx
@@ -1,5 +1,38 @@
 import React, { PropTypes } from 'react';
 
+const formStyles = {
+  position: 'absolute',
+  backgroundColor: '#D4DDE7',
+  border: '1px solid #133253',
+  margin: '-75px 0 0 -125px',
+  maxWidth: '250px',
+  height: '150px',
+  padding: '20px',
+  left: '50%',
+  top: '50%',
+}
+
+const inputStyles = {
+  border: '1px solid #f9f9f9',
+  padding: '10px',
+  width: '100%',
+  textAlign: 'right',
+  marginBottom: '2px'
+}
+
+let buttonStyles = {
+  backgroundColor: '#2B4C6F',
+  border: 'none',
+  padding: '5px',
+  color: '#fff',
+  width: '50%',
+}
+
+let cancelButtonStyles = Object.assign({}, buttonStyles, {
+  backgroundColor: '#A5B7CA'
+})
+
+
 const BuyItemForm = ({ openDialog, listId, id, onCloseDialog, onSubmit }) => {
   let price;
   let quantity;
@@ -7,14 +40,14 @@ const BuyItemForm = ({ openDialog, listId, id, onCloseDialog, onSubmit }) => {
   return (
     <div>
       {openDialog ? (
-      <form onSubmit={e => {
+      <form style={formStyles} onSubmit={e => {
           e.preventDefault();
           onSubmit(listId, id, quantity.value, price.value);
         }}>
-        <input ref={node => {quantity = node}} type={"number"} placeholder={"quantidade"} />
-        <input ref={node => {price = node}} placeholder={"preço"} />
-        <button type={"submit"}>comprar</button>
-        <button type={"button"} onClick={onCloseDialog}>X</button>
+        <input ref={node => {quantity = node}} type={"number"} placeholder={"quantidade"} style={inputStyles} />
+        <input ref={node => {price = node}} placeholder={"preço"} style={inputStyles} />
+        <button type={"submit"} style={buttonStyles}>comprar</button>
+        <button type={"button"} onClick={onCloseDialog} style={cancelButtonStyles}>cancelar</button>
       </form> ) : null }
     </div>
   )
