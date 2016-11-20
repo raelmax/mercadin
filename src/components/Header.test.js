b76d8485a82c37f13a53dda92cbee3e8a9ac071f
Rael Max <rael.nascimento@corp.globo.com>
2016-11-20 13:20:38 -0200

    Update packages and add tests to Header Component

--- src/components/Header.test.js
+++ src/components/Header.test.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import Header from './Header.jsx';
+import Header from './Header';
 
-describe('Header Component', () => {
+describe('Component: <Header />', () => {
   it('renders without crashing', () => {
     shallow(<Header />);
   });
 
+  it('renders with correct text', () => {
+    const header = shallow(<Header text="Test Header" />);
 
+    expect(header.find('h1').text()).toEqual('Test Header');
+  });
+
+  it('renders back button if necessary', () => {
+    const header = shallow(<Header text="Test Header" backButton={true} />);
+    expect(header.find('.header-back-button').length).toEqual(1);
+  });
 });
