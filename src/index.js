7e07a473efbd49741da57c20156df64c81c71063
Rael Max <rael.nascimento@corp.globo.com>
2016-11-21 13:02:13 -0200

    Move from hashHistory to browserHistory

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-import { Router, Route, hashHistory } from 'react-router';
+import { Router, Route, browserHistory } from 'react-router';
 import { syncHistoryWithStore, routerMiddleware } from 'react-router-redux';
 
 import './app.css';
@@ -11,11 +11,11 @@ import ShoppingList from './components/ShoppingList';
 import ShoppingListDetail from './components/ShoppingListDetail';
 import shoppingListApp from './reducers/Index';
 
-const middleware = routerMiddleware(hashHistory)
+const middleware = routerMiddleware(browserHistory)
 let store = createStore(shoppingListApp, applyMiddleware(middleware));
 
 // Create an enhanced history that syncs navigation events with the store
-const history = syncHistoryWithStore(hashHistory, store)
+const history = syncHistoryWithStore(browserHistory, store)
 
 ReactDOM.render(
   <Provider store={store}>
